bb73cbf299439168307de01cef4ee75c4387bd83
seungchan0324 <winchan324@gmail.com>
2025-03-12 10:44:06 +0900

    edit filename

--- supplement/ex_selenium.py
+++ supplement/ex_selenium.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from supplement.ex_selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
--- supplement/movement_path.py
+++ supplement/movement_path.py
@@ -7,7 +7,7 @@ def get_movement_path(departure, arrival):
     with sync_playwright() as p:
 
         browser = p.chromium.launch(headless=False)
-        context = browser.new_context(viewport={"width": 1920, "height": 1680})
+        context = browser.new_context(viewport={"width": 1920, "height": 1080})
         page = context.new_page()
 
         page.goto("https://www.google.co.kr/maps/")
